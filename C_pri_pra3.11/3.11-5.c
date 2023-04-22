5df5ba2a2d1d09bd7e7872a7a0d77e9f64d0983f
Bro-nya <112709184+Bro-nya@users.noreply.github.com>
2023-04-22 16:32:50 +0800

    Add files via upload

---
+++ C_pri_pra3.11/3.11-5.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	int year = 3.156E7;
+	int age = 0;
+	printf("«Î ‰»ÎƒÍ¡‰\n");
+	scanf("%d", &age);
+	printf("%d", age * year);
+	return 0;
+}
