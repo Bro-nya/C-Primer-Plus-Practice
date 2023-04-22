003906f4b37a5e01bc092038fe4ad8edf5810f97
Bro-nya <112709184+Bro-nya@users.noreply.github.com>
2023-04-22 16:16:36 +0800

    Add files via upload

---
+++ C_pri_pra2.12/2.12-6.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int toes = 10;
+	printf("双倍为%d\n", toes * 2);
+	printf("平方为%f", pow(toes, 2));
+	return 0;
+}
